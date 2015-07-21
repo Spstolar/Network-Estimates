e7d85c6f59f902d1d034ee0c218d34c37bdee9af
Spstolar <simonstolarczyk@Simons-MacBook-Pro.local>
2015-07-21 16:27:03 -0500

    Updates connectionsforvertex
    Adds a function description and cleans up a couple of lines.

--- connectionsforvertex.m
+++ connectionsforvertex.m
@@ -1,7 +1,11 @@
 function[ x ] = connectionsforvertex( a , M)
+% takes a vector which encodes who the first entry agent sends info to and
+% the total number of agents M and turns this into the matrix which is
+% empty except for the column corresponding to that agent.
+% [3 5 8] means agent 3 sends info to agents 5 and 8.
+
 x = zeros(M,M);
-a(1);
-[O N] = size(a);
+[~, N] = size(a);
 for k = 2:N
     x(a(k),a(1)) = 1;
 end
